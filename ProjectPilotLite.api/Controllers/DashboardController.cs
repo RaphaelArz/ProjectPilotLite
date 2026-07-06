5a96600c0fcd4a23f22cf8b252895472b526439a
Raphael <raphaelarzoin173@gmail.com>
2026-07-06 16:03:46 +0200

    creation du controller de dashboard pour avoir l'ensembles des informations globales

---
+++ ProjectPilotLite.api/Controllers/DashboardController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProjectPilotLite.Api.Data;
+using ProjectPilotLite.Core.Dtos;
+using ProjectPilotLite.Core.Enums;
+using System.Threading.Tasks;
+
+namespace ProjectPilotLite.Api.Controllers;
+
+[ApiController]
+[Route("api/dashboard")]
+public class DashboardController : ControllerBase
+{
+    private readonly AppDbContext _db;
+
+    public DashboardController(AppDbContext db)
+    {
+        _db = db;
+    }
+
+
+    /// <summary>
+    /// Récupère les differentes infos concernant les projets (nb projet, total statut ect..)
+    /// </summary>
+    /// <response code="200">envoie des informations</response>
+    [HttpGet]
+    public async Task<ActionResult<DashboardDto>> GetDashboard()
+    {
+        var dashboard = new DashboardDto
+        {
+            TotalProjects = await _db.Projects.CountAsync(),
+            ProjectsInProgress = await _db.Projects.CountAsync(p => p.Status == ProjectStatus.InProgress),
+            ProjectsBlocked = await _db.Projects.CountAsync(p => p.Status == ProjectStatus.Blocked),
+            TotalTasks = await _db.Tasks.CountAsync(),
+            CompletedTasks = await _db.Tasks.CountAsync(t => t.Status == ProjectTaskStatus.Done),
+            SubmittedDeliverables = await _db.Deliverables.CountAsync(d => d.Status == DeliverableStatus.Submitted)
+        };
+
+        return Ok(dashboard);
+    }
+}
